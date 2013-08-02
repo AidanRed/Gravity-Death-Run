1ce619d2591b1df07e5f344eabb33ea2aa46d772
Aidan Redding <ToucanMarooney@gmail.com>
2013-08-02 18:21:17 +0930

    Formatting changes
    Changed tabs to spaces, and did some minor formatting changes to make
    the code more readable.

--- mainMenu.py
+++ mainMenu.py
@@ -1,15 +1,15 @@
 import pyglet
 from pyglet.gl import *
+
 windowWidth = 640
 windowHeight = 480
-gameWindow = pyglet.window.Window(windowWidth,windowHeight)
+gameWindow = pyglet.window.Window(windowWidth, windowHeight)
 FPS = 60
-
 
 
 #Squares to be drawn on-screen in the background. 
 class Square:
-    def __init__(self,x,y,height,width,columnColour):
+    def __init__(self, x, y, height, width, columnColour):
         self.x = x
         self.y = y
         self.height = height
@@ -18,25 +18,30 @@ class Square:
         self.colour = columnColour
         #Shade. random number to start with, gradually increases to 255 then after a pause returns to 0 and gradually increases again
         self.shade = 1
-        
+
     def drawSelf(self):
-        pyglet.graphics.draw(4, GL_QUADS,('v2i', (self.x,self.y+self.height, self.x+self.width,self.y+self.height, self.x+self.width,self.y, self.x,self.y)),('c3B', (self.colour[0],self.colour[1],self.colour[2],
-                                                                                                          self.colour[0],self.colour[1],self.colour[2],
-                                                                                                          self.colour[0],self.colour[1],self.colour[2],
-                                                                                                          self.colour[0],self.colour[1],self.colour[2],)))
-        
+        pyglet.graphics.draw(4, GL_QUADS, ('v2i', (
+        self.x, self.y + self.height, self.x + self.width, self.y + self.height, self.x + self.width, self.y, self.x,
+        self.y)), ('c3B', (self.colour[0], self.colour[1], self.colour[2],
+                           self.colour[0], self.colour[1], self.colour[2],
+                           self.colour[0], self.colour[1], self.colour[2],
+                           self.colour[0], self.colour[1], self.colour[2],)))
+
+
 squares = []
-squares.append(Square(4,6,65,43,(56,57,58)))
+squares.append(Square(4, 6, 65, 43, (56, 57, 58)))
+
 
 @gameWindow.event
 def on_draw():
     gameWindow.clear()
     for square in squares:
         square.drawSelf()
-	
-    
+
+
 def update(dt):
     pass
-    
-pyglet.clock.schedule_interval(update, 1.0/120.0)    
+
+
+pyglet.clock.schedule_interval(update, 1.0 / 120.0)
 pyglet.app.run()
