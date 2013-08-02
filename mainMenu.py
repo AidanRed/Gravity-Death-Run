d30daf494f6232295fab17791891749ce4ba8879
TehDarkMiner <andrewschom@live.com>
2013-08-02 19:56:28 +0930

    Column colours
    Added column colours to the main menu. Will need to make it select from
    a larger selection of colours and slowly fade between colours.

--- mainMenu.py
+++ mainMenu.py
@@ -1,4 +1,4 @@
-import pyglet
+import pyglet, random
 from pyglet.gl import *
 
 windowWidth = 640
@@ -29,10 +29,53 @@ class Square:
                            self.colour[0], self.colour[1], self.colour[2],)))
 
 
+#Create the squares and set them to a grid constrained to the screen size.
 squares = []
 for i in range(windowWidth/squareSize):
     for ii in range(windowHeight/squareSize):
 		squares.append(Square(i*squareSize, ii*squareSize, squareSize, squareSize, (56, 57, 58)))
+		
+#This method of setting colours for columns may be deemed inefficient and may not be used.
+#Choose the colours from a list of colours. 
+
+lblue = (51,255,255)
+blue = (0,0,255)
+dblue = (0,0,102)
+red = (255,0,0)
+pink = (255,51,153)
+purple = (153,51,255)
+lgreen = (204,255,0)
+green = (51,255,0)
+dgreen = (0,102,0)
+yellow = (255,255,255)
+orange = (153,255,51)
+
+colours = []
+colours.append(lblue)
+colours.append(blue)
+colours.append(dblue)
+colours.append(red)
+colours.append(pink)
+colours.append(purple)
+colours.append(green)
+colours.append(dgreen)
+colours.append(lgreen)
+colours.append(yellow)
+colours.append(orange)
+
+possibleColours = []
+for i in range(windowWidth/squareSize):
+	possibleColours.append(colours[random.randint(0,10)])
+	print i
+
+for i in range(windowWidth/squareSize):
+	for ii in squares:
+		if i*squareSize == ii.x:
+			ii.colour = possibleColours[i]
+			
+	
+
+
 
 
 @gameWindow.event
